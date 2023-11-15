7215f7df2e4f1fa8ca20c926d170af1ab54c1a76
Mark-Kil <markkilcanarias@gmail.com>
2023-11-15 20:53:14 +0100

    page 3 list is now made with flex, array reading is reversed

--- src/pages/results/ts/fillResults.ts
+++ src/pages/results/ts/fillResults.ts
@@ -3,10 +3,10 @@ import { retreiveStoredEliminatedPlayers } from "./storedEliminatedList"
 let resultsDiv = document.getElementById("resultsContainer")
 
 export function fillResults() {
-    retreiveStoredEliminatedPlayers().forEach(function(element, index){
+    retreiveStoredEliminatedPlayers().reverse().forEach(function(element, index){
         resultsDiv.innerHTML += /* html */ `
         <div class="PlayerDiv" id="Player${index + 1}">
-        <p class="roundText" id="round${index + 1}">Round ${index + 1}: </p>
+        <p class="roundText" id="round${index + 1}">Round ${retreiveStoredEliminatedPlayers().length - (index)}: </p>
             <div class="nameAndAvatarContainer" id="nameAndAvatarContainer${index + 1}">
                 <p class="PlayerName" id="playerName${index + 1}">${element.name}</p>
                 <img class="Avatar" src=${element.avatar} alt="avatar1">
