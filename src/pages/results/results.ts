f557ba043a63c3b26e5c2bdd0f39d57aeca74b60
Mark-Kil <markkilcanarias@gmail.com>
2023-11-15 18:52:49 +0100

    buttons on page 3 working

--- src/pages/results/results.ts
+++ src/pages/results/results.ts
@@ -1,9 +1,15 @@
+import { backButton } from '../../assets/tsModules/backButton'
+import { newGameButton } from '../../assets/tsModules/newGameButton'
+import { playAgainButton } from '../../assets/tsModules/playAgainButton'
 import '../../style.scss'
 import './scss/results.scss'
 import { fillResults } from './ts/fillResults'
 
 function results() {
     fillResults()
+    backButton()
+    playAgainButton()
+    newGameButton()
 }
 
 results()
