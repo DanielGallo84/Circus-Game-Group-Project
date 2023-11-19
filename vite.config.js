97082a01731300d2962742d79da3a808a26684e7
Mark-Kil <markkilcanarias@gmail.com>
2023-11-19 14:40:29 +0100

    project base added to config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig ({
+    base: '/Circus-Game-Group-Project/',
     root: 'src',
     publicDir: './public',
     build: {
